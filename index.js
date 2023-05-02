38bb1280298d99e48437a6bced13423826a2cbec
safayet <safayetdib@gmail.com>
2023-05-03 02:24:30 +0600

    updated chefs data

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+var cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 const data = require('./data/data.json');
 
 app.get('/data', (req, res) => {
 	res.send(data);
+});
+
+app.get('/data/:id', (req, res) => {
+	const id = parseInt(req.params.id);
+	const selectedData = data.find((d) => d.id === id);
+	res.send(selectedData);
 });
 
 app.listen(port, () => {

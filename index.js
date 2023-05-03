d7740cef0af40d2c777f5eecf1242340c2e1fb6a
safayet <safayetdib@gmail.com>
2023-05-03 21:08:45 +0600

    updated data

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const express = require('express');
-var cors = require('cors');
+const cors = require('cors');
+const data = require('./data/data.json');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-const data = require('./data/data.json');
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
 
 app.get('/data', (req, res) => {
 	res.send(data);
 });
 
-// app.get('/data/:id', (req, res) => {
-// 	const id = req.params?.id;
-// 	const selectedData = data.find((d) => d.id == id);
-// 	res.send(selectedData);
-// });
+app.get('/data/:id', (req, res) => {
+	const id = req.params?.id;
+	const selectedData = data.find((d) => d.id == id);
+	res.send(selectedData);
+});
 
 app.listen(port, () => {
 	console.log(`This app listening on port ${port}`);
